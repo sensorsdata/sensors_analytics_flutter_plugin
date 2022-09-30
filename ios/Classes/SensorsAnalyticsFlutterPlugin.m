00cc3d86e4311f6ecd08129f7eff8c3d96df6561
吴攀 <wupan@sensorsdata.cn>
2022-09-30 15:09:02 +0800

    【feature】使用 Flutter SDK ，$appStart 无法上报注册的公共属性

--- ios/Classes/SensorsAnalyticsFlutterPlugin.m
+++ ios/Classes/SensorsAnalyticsFlutterPlugin.m
@@ -1,5 +1,26 @@
+//
+// SensorsAnalyticsFlutterPlugin.m
+// sensors_analytics_flutter_plugin
+//
+// Created by  储强盛 on 2022/9/14.
+// Copyright © 2015-2022 Sensors Data Co., Ltd. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 #import "SensorsAnalyticsFlutterPlugin.h"
 #import "SensorsAnalyticsSDK.h"
+#import "SAFlutterGlobalPropertyPlugin.h"
 #import <objc/runtime.h>
 
 static NSString* const SensorsAnalyticsFlutterPluginMethodTrack = @"track";
@@ -488,7 +509,13 @@ static NSString* const SensorsAnalyticsFlutterPluginMethodInit = @"init";
             options.enableVisualizedProperties = [visualizedSettings[@"properties"] boolValue];
         }
     }
-    
+
+    NSDictionary *properties = config[@"globalProperties"];
+    if ([properties isKindOfClass:NSDictionary.class]) {
+        SAFlutterGlobalPropertyPlugin *propertyPlugin = [[SAFlutterGlobalPropertyPlugin alloc] initWithGlobleProperties:properties];
+        [options registerPropertyPlugin:propertyPlugin];
+    }
+
     // 开启 SDK
     [SensorsAnalyticsSDK startWithConfigOptions:options];
 }
