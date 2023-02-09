03ae899fb3d582606ea5ff09e88850519596a613
weiqiangliu <liuweiqiang@sensorsdata.cn>
2023-02-09 15:51:56 +0800

    Release 2.3.1

--- lib/sensors_analytics_flutter_plugin.dart
+++ lib/sensors_analytics_flutter_plugin.dart
@@ -37,7 +37,7 @@ class VisualizedConfig {
 
 // This is the official Flutter Plugin for Sensors Analytics.
 class SensorsAnalyticsFlutterPlugin {
-  static const String FLUTTER_PLUGIN_VERSION = "2.3.0";
+  static const String FLUTTER_PLUGIN_VERSION = "2.3.1";
   static bool hasAddedFlutterPluginVersion = false;
 
   static Future<String?> get getDistinctId async {
